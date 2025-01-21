6d53eddf909c42038f41a85055adcd863d3bc206
daantes <danik230705@gmail.com>
2025-01-21 18:48:14 +0000

    new ids

--- src/index.js
+++ src/index.js
@@ -48,6 +48,49 @@ document.getElementById("saveBtn").addEventListener("click", () => {
       price: document.getElementById("bitumen-cleaning-price").value,
     },
     // TODO: add other fields
+    outsidePolish: {
+      partial: {
+        checked: document.getElementById("out-partial-check").value,
+        price: document.getElementById("out-partial-price").value,
+        elements: {
+          frontBumper: document.getElementById("out-front-bumper").value,
+          hood: document.getElementById("out-hood").value,
+          roof: document.getElementById("out-roof").value,
+          trunk: document.getElementById("out-trunk").value,
+          rearBumper: document.getElementById("out-rear-bumper").value,
+          leftFrontWing: document.getElementById("out-left-front-wing").value,
+          rightFrontWing: document.getElementById("out-right-front-wing").value,
+          leftRearWing: document.getElementById("out-left-rear-wing").value,
+          rightRearWing: document.getElementById("out-right-rear-wing").value,
+          rightRearDoor: document.getElementById("out-right-rear-door").value,
+          rightFrontDoor: document.getElementById("out-right-front-door").value,
+          leftFrontDoor: document.getElementById("out-left-front-door").value,
+          leftRearDoor: document.getElementById("out-left-rear-door").value,
+        },
+      },
+      full: {
+        checked: document.getElementById("out-full-check").value,
+        price: document.getElementById("out-full-price").value,
+      },
+    },
+    detailsCleaning: {
+      checked: document.getElementById("out-elements-check").value,
+      price: document.getElementById("out-elements-price").value,
+    },
+    decorativeTipCleaning: {
+      checked: document.getElementById("out-decorative-tip-check").value,
+      price: document.getElementById("out-decorative-tip-price").value,
+    },
+    wheelsCleaning: {
+      checked: document.getElementById("out-wheels-check").value,
+      price: document.getElementById("out-wheels-price").value,
+    },
+    underHoodCleaning: {
+      checked: document.getElementById("out-under-hood-check").value,
+      price: document.getElementById("out-under-hood-price").value,
+    },
+    date: document.getElementById("date").value,
+    manager: document.getElementById("manager").value,
   };
 
   pdfGenerator.generatePDF(data);
